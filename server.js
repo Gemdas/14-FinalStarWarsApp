20a5b121aa010d611ac459d074eb6ec2f7067fa5
Gemdas <Brennan.artzt@gmail.com>
2017-10-25 21:25:26 -0500

    fix everthing?

--- server.js
+++ server.js
@@ -89,6 +89,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(process.env, function() {
   console.log("App listening on PORT " + PORT);
 });
